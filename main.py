b8d4c2e8cfc836a48ada64c37e8f41cbff162e3d
KrzysztofKaczan <wrx92912@student.wroclaw.merito.pl>
2025-04-12 21:00:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from turtle import Screen, Turtle
+import time
+from snake import Snake 
+
 
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake Gra PL Super Chomikuj.pl")
+screen.tracer(0)
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right,"Right")
+screen.onkey(snake.left,"Left")
 
-segment_1 = Turtle("square")
-segment_1.color("white")
-
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20,0)
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(-30,0)
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    
 screen.exitonclick()
