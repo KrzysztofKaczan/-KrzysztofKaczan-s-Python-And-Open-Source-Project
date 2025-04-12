e012bc90d5b524fa9162f7469632880a7391c412
KrzysztofKaczan <wrx92912@student.wroclaw.merito.pl>
2025-04-12 19:54:08 +0200

    Add files via upload
    coś tam poprawione i tak dalej

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake Gra PL Super Chomikuj.pl")
+screen.tracer(0)
 
+starting_positions = [(0,0), (-20,0), (-40,0)]
 
-segment_1 = Turtle("square")
-segment_1.color("white")
+segments = []
+for position in starting_positions:
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20,0)
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(-30,0)
-screen.exitonclick()
+    segment_new = Turtle("square")
+    segment_new.color("white")
+    segment_new.penup()
+    segment_new.goto(position)
+    segments.append(segment_new)
+   
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) -1, 0, -1):
+         new_x =  segments[seg_num - 1].xcor()
+         new_y =  segments[seg_num - 1].ycor()
+         segments[seg_num].goto(new_x,new_y)
+    segments[0].forward(20)        
+    
